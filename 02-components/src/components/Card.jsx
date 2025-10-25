172cf9b6a3fc7b8d993bc894435df8c3d33e69cf
Dhruv Sood <dhruvsood1102@gmail.com>
2025-10-26 02:00:55 +0530

    02 and 03 folder done

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <h1>Dhruv Sood</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis,
+        reiciendis!
+      </p>
+    </div>
+  );
+};
+
+export default Card;

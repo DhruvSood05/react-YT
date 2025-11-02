18e65257ed06765daacfb7022f98784d7fd2e332
Dhruv Sood <dhruvsood1102@gmail.com>
2025-11-02 22:58:18 +0530

    Code not complete yet React router dom

---
+++ 19-routing advnaced/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className=" flex py-4 px-8 bg-cyan-950 justify-between text-white">
+      <h2 className="text-xl font-bold">ReactJS</h2>
+      <div className=" flex gap-8">
+        <Link to="/" className="text-lg font-bold">
+          Home
+        </Link>
+        <Link to="/about" className="text-lg font-bold">
+          About
+        </Link>
+        <Link to="/product" className="text-lg font-bold">
+          Product
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
